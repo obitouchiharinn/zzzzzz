c4154bfa4f533b0822385c227516c8709a0b6b9d
obitouchiharinn <sachin2004164@gmail.com>
2025-09-10 19:12:26 +0530

    google login

--- agency-ai/src/components/auth/Login.jsx
+++ agency-ai/src/components/auth/Login.jsx
@@ -51,7 +51,8 @@ const Login = () => {
   const handleGoogleLogin = async () => {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
-      options: { redirectTo: "http://localhost:5173/" },
+      // options: { redirectTo: "http://localhost:5173/" },
+      options: { redirectTo: "https://zzzzzz-sand.vercel.app/" },
     });
     if (error) {
       console.error("Error logging in:", error.message);
